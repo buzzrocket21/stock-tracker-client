922cdd7150042e4bc13db3667b62641b3a63d450
Frank Maiolo <buzzrocket21@gmail.com>
2020-05-30 06:04:40 -0400

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Client, Message } from '@stomp/stompjs';
+
 import './App.css';
+import { StockModel } from './StockModel';
+
+const client = new Client({
+  brokerURL: 'ws://localhost:8085/stock-tracker/websocket',
+  connectHeaders: {
+    connection: 'upgrade',
+    upgrade: 'websocket'
+  }
+});
 
 function App() {
+  const [connected, setConnected] = useState(false);
+  const [stockMap, setStockMap] = useState(new Map<string, StockModel>());
+
+  useEffect(() => {
+    // keeps trying to reconnect until connected
+    if(connected) {
+      return;
+    }
+    client.onConnect = (e) => {
+      setConnected(true);
+
+      client.subscribe('/topic/public', (message: Message) => {
+        if(message.body) {
+          const stock = JSON.parse(message.body);
+          stockMap.set(stock.key, stock);
+          setStockMap(new Map<string, StockModel>(stockMap));
+        }
+      });
+
+      client.publish({
+        destination: '/app/track',
+        body: 'intl'
+      });
+    };
+
+    client.activate();
+
+    return () => {
+      // client.deactivate();
+    }
+  }, [connected, stockMap]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {connected ? 'true' : 'false'}
+      <ul>
+      {Array.from(stockMap.entries()).map(([key, value]: [string, StockModel]) => {
+        return <li key={key}>{`${key}: ${JSON.stringify(value)}`}</li>;
+      })}
+      </ul>
     </div>
   );
 }
---
+++ src/StockModel.ts
@@ -0,0 +1,4 @@
+export class StockModel {
+    key!: string;
+    price!: number;
+}
